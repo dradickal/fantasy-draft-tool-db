e34c00e209481cf0e73bf608545fb840962badf5
David Radick <dradick@radickalcreations.com>
2025-07-24 03:38:21 -0500

    latest build with settings dialog

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,25 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import vitePluginSass from 'vite-plugin-sass';
+import sass from 'sass';
 
 export default defineConfig({ 
     plugins: [
-        vitePluginSass(),
         react(),
     ],
+    css: {
+        preprocessorOptions: {
+            scss: {
+                implementation: sass, 
+            },
+        },
+    },
     build: {
         rollupOptions: {
             external: ['window'],
-        } 
+            output: {
+                sourcemap: false,
+            }
+        },
+        sourcemap: false, 
     }
 });
